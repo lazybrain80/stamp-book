726615cf9c750d50f2d4d3d6b88bd673807b20e0
lazybrain80 <lazybrain80@gmail.com>
2024-08-29 14:48:40 +0900

    랜딩 페이지 첫 번째 설명 변경

--- apps/nextjs/src/components/marketing/row-1st.tsx
+++ apps/nextjs/src/components/marketing/row-1st.tsx
@@ -8,20 +8,19 @@ export default function MarketingShow() {
   return (
     <div className="mx-auto grid w-full grid-cols-1 gap-4 lg:grid-cols-3">
       <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
-        <div className="max-w-sm">
-          <h2 className="max-w-sm text-balance  text-left text-base font-semibold tracking-[-0.015em] text-white md:max-w-lg md:text-xl lg:text-3xl">
-          맞춤형 워터마크 만들기
+        <div className="max-w-lg">
+          <h2 className="max-w-lg text-balance text-left text-base font-semibold tracking-[-0.015em] text-white md:max-w-lg md:text-xl lg:text-3xl">
+            보이지 않는 워터마크란 무엇인가요?
           </h2>
-          <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-          브랜드를 인식하게 만드세요. 사진, 아트워크, 프로모션 자료에 사용할 맞춤형 워터마크를 만들어 사람들로 하여금 지금 보고 있는 브랜드가 무엇인지 알게 하세요. 로고, 이름, 마스코트, 서명 등 브랜드를 확인할 수 있는 표식을 사용해 보세요. Canva(캔바)의 광범위한 글꼴 라이브러리에서 선택하여 글꼴을 맞춤화하세요. 그런 다음, 워터마크를 다운로드하여 모든 사진에 사용하세요.
+          <p className="mt-4 max-w-3xl text-left text-lg text-base/6 text-neutral-200">
+            이미지나 비디오 등의 콘텐츠에 삽입되어, 눈에 보이지 않지만 특정 정보를 숨겨주는 기법입니다. 일반적으로 정보 삽입, 저작권 보호, 추적 기능과같은 기능을 수행합니다.
           </p>
         </div>
         <img
-          src="https://ui.aceternity.com/_next/image?url=%2Flinear.webp&w=1080&q=75"
-          width={500}
-          height={500}
+          src="/images/marketing/cat_invisible_wm.png"
+          width={600}
           alt="linear demo"
-          className="absolute -bottom-10 -right-10 rounded-2xl object-contain md:-right-[40%] lg:-right-[20%]"
+          className="absolute bottom-0 right-10 rounded-2xl object-contain"
         />
       </WobbleCard>
     </div>
