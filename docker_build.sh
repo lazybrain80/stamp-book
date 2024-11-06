a7f7a28b0e4e8d382e0cac20888c3cc306bfeb25
lazybrain80 <lazybrain80@gmail.com>
2024-11-06 19:38:11 +0900

    도커 arm64 빌드 스크립트 추가

--- docker_build.sh
+++ docker_build.sh
@@ -1,1 +1,7 @@
+// base build
 docker build -t stamp-book:1.0 .
+
+// arm64 build
+docker buildx create --name stampArm64Builder --use
+
+docker buildx build --platform linux/arm64 -t ghcr.io/lazybrain80/stamp-book:1.0 --push .
