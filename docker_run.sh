1f88e75c09feb2bd49b191b89c6635239f0a6639
lazybrain80 <lazybrain80@gmail.com>
2024-11-08 17:14:02 +0900

    docker run 명령어 변경

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
-docker run -dit -p 4444:4444 --name stamp-book stamp-book:1.0
+docker run -dit -p 4444:4444 --name stamp-book ghcr.io/lazybrain80/stamp-book:1.0
 
 //집
 docker run -itd --name stamp-book-sh\
